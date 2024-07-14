0d100ff5c9d992a163cd45d993702a74e586d18b
KrishKhattar <krish.khattar@gmail.com>
2024-07-14 23:17:09 +0530

    day 2 task added

---
+++ Day-2/feature-request/index.js
@@ -0,0 +1,25 @@
+// Feature request 1
+
+let num1 = 100;
+let num2 = 200;
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+console.log(sum, difference, product, quotient);
+
+// Feature request 2
+
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+console.log(num1 === num2);
+console.log(num1 !== num2);
+console.log(num1 > num2 && num1 < num2);
+console.log(num1 > num2 || num1 < num2);
+console.log(!(num1 > num2));
+
+// Feature request 3
+
+console.log(num1 >= 0 ? 'num is positive' : 'num is negative');
---
+++ Day-2/task/index.js
@@ -0,0 +1,83 @@
+//Activity 1: Arithmetic Operators
+// Task 1
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+
+console.log(sum);
+
+// Task 2:
+let num3 = 30;
+let num4 = 40;
+let difference = num3 - num4;
+console.log(difference);
+
+// Task 3
+let num5 = 50;
+let num6 = 60;
+let product = num5 * num6;
+console.log(product);
+
+// Task 4
+let num7 = 70;
+let num8 = 80;
+let quotient = num7 / num8;
+console.log(quotient);
+
+// Task 5
+let num9 = 90;
+let num10 = 100;
+let remainder = num9 % num10;
+console.log(remainder);
+
+// Activity 2: Assignment Operator
+
+// Task 6
+let num11 = 100;
+num11 += 120;
+console.log(num11);
+
+// Task 7
+let num12 = 200;
+num12 -= 100;
+console.log(num12);
+
+// Activity 3: Comparison Operators
+
+// Task 8
+let num13 = 300;
+let num14 = 400;
+console.log(num13 > num14);
+console.log(num13 < num14);
+
+// Task 9
+let num15 = 500;
+let num16 = 600;
+console.log(num15 >= num16);
+console.log(num15 <= num16);
+
+// Task 10
+let num17 = 700;
+let num18 = '800';
+console.log(num17 == num18);
+console.log(num17 === num18);
+
+// Activity 4: Logical Operators
+
+// Task 11
+let num19 = 900;
+let num20 = 1000;
+console.log(num19 > num20 && num19 < num20);
+
+// Task 12
+console.log(num19 > num20 || num19 < num20);
+
+// Task 13
+console.log(!(num19 > num20));
+
+// Activity 5: Ternary Operator
+
+// Task 14
+let num21 = 1100;
+let result = num21 > 0 ? 'Positive' : 'Negative';
+console.log(result);
